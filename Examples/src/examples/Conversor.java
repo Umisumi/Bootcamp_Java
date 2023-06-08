57954fb8259c4ca8ac66e0a593cf8008b47b8e52
Morita <Morita@DESKTOP-9N7BS8G>
2023-06-07 20:43:55 -0500

    Ejemplos prácticos.

---
+++ Examples/src/examples/Conversor.java
@@ -0,0 +1,27 @@
+package examples;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Morita
+ */
+public class Conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Conversor de millas a kilómetros
+
+        Scanner input = new Scanner(System.in);
+        double millas, kilometros;
+
+        //cadena = JOptionPane.showInputDialog("Hola Mundo");
+        System.out.println("Distancia en millas: ");
+        millas = input.nextDouble();
+        kilometros = millas * 1.60934;
+        System.out.println("La converción a kilómetros es: " + kilometros);
+    }
+
+}
